efe25cb90b051ae4c5a2631c896c70bec031d5a3
shiiotaka <shiiotaka@gmail.com>
2021-06-07 12:30:08 +0900

    Add: user_sessionsルーティングの作成 #6

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'static_pages#home' # rootディレクトリ
 
   resources :users, only: %i[index new create]
+  resources :user_sessions, only: %i[new create destroy]
 end
