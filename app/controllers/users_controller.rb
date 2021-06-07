f1cba988aeb5b733b4f300951faa565fc3774bbd
shiiotaka <shiiotaka@gmail.com>
2021-06-07 15:03:34 +0900

    Add: #6 users_controllerの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
 
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
   private
 
+  # ストロングパラメーター
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
