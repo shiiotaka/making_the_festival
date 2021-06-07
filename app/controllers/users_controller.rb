b6c6832d86f5956a376849e37025ee4a05c9c330
shiiotaka <shiiotaka@gmail.com>
2021-06-07 15:24:47 +0900

    Add: #6 controllerの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,21 @@ class UsersController < ApplicationController
   def index
   end
 
+  # GET /users/new Prefix => new_user
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    # @userのデータ保存に成功するとき、login_pathにリダイレクトする
+    # falseの場合、newアクションにrenderする
+    if @user.save
+      redirect_to login_path, notice: 'ユーザー登録が完了しました'
+    else
+      render :new
+    end
   end
 
   private
