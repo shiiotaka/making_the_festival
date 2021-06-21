6522f0cebc8dc1602939a3599523abb2ccd2120d
shiiotaka <shiiotaka@gmail.com>
2021-06-21 11:49:22 +0900

    Add: コメントの記載

--- app/models/time_table.rb
+++ app/models/time_table.rb
@@ -1,9 +1,9 @@
 class TimeTable < ApplicationRecord
-  has_many :schedules # 複数のScheduleを持っている
+  has_many :schedules # 複数のScheduleを持っている(中間テーブル)
   has_many :artists, through: :schedules # 複数のArtistを持っていて、中間モデルにSchedulesがある
   belongs_to :user # Userモデルに属している
 
-  validates_associated :artists
-  accepts_nested_attributes_for :artists
+  validates_associated :artists # Artistモデルのバリデーションを実行
+  accepts_nested_attributes_for :artists # Artistモデルを更新、削除刷ることができる
 
 end
