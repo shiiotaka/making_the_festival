14ffbfd7adb8ba0494d143d509091b138cffdf1c
shiiotaka <shiiotaka@gmail.com>
2021-07-01 21:09:50 +0900

    Add: layoutの追加

--- app/controllers/time_tables_controller.rb
+++ app/controllers/time_tables_controller.rb
@@ -23,8 +23,9 @@ class TimeTablesController < ApplicationController
 
   # GET /time_tables/:id
   def show
+    @user = User.find(params[:id])
     @time_table = TimeTable.find(params[:id])
-    @artists = @time_table.artists 
+    @artists = @time_table.artists
   end
 
   private
